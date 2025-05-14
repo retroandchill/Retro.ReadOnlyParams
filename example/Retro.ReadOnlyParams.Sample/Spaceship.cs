13312c108adae9eb534030a02f95abe265d3ad6b
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-05-14 18:33:02 -0400

    Got the reassignment error to report

--- example/Retro.ReadOnlyParams.Sample/Spaceship.cs
+++ example/Retro.ReadOnlyParams.Sample/Spaceship.cs
@@ -1,9 +1,11 @@
 using System;
+using Retro.ReadOnlyParams.Annotations;
 
 namespace Retro.ReadOnlyParams.Sample;
 
 public class Spaceship {
-  public void SetSpeed(long speed) {
+  public void SetSpeed([ReadOnly] long speed) {
+    speed = 4;
     if (speed > 299_792_458)
       throw new ArgumentOutOfRangeException(nameof(speed));
   }
